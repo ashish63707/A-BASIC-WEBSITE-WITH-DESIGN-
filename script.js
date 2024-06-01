3264bf838084463c7124ced4bddfc08306a40e97
Ashish swain <98210791+ashish63707@users.noreply.github.com>
2024-06-01 15:09:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('.dropdown').hover(function () {
+        $('.dropdown-content', this).toggle();
+    });
+
+
+    $('#signin-form').submit(function (event) {
+
+    });
+});
